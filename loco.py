c370e4fce0370dff8604706e4b6fbed1d6637d10
ritupratik <47660530+ritupratik@users.noreply.github.com>
2019-02-23 15:54:13 +0530

    Update loco.py

--- loco.py
+++ loco.py
@@ -22,5 +22,10 @@ async def on_member_join(member):
 @client.event    
 async def on_ready():
     await client.change_presence(game=Game(name='LOCO,HQ,BB//TRIVIA ROCK'))
-    print('Ready, Freddy') 
+    print('Ready, Freddy')
+    
+@client.event
+async def on_message(message):
+    if message.content == '₹no':
+        await client.send_message(message.channel,'ok no problem You can contact us anytime')
 client.run('NTQ4NzkyMjczNDYzNDEwNjk5.D1KpCg.G1cuZ1HJnae0cIVDwhqQa9ag-WE')
